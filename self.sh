a29cb425928cd4cce5c322a3a2a011157cd36e3d
Andre Blanke <blanke.and@gmail.com>
2022-06-23 20:18:45 +0200

    Add two utility scripts for verification of class correctness and auto-obfuscation

---
+++ self.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+#
+# Tests whether the obfuscator can obfuscate itself.
+
+JAR_ARTIFACT_PATH='target/indy-obfuscator-1.0-SNAPSHOT.jar'
+OBF_ARTIFACT_PATH='target/indy-obfuscator-1.0-SNAPSHOT.obf.jar'
+
+if [ ! -f "$(dirname "$0")/$JAR_ARTIFACT_PATH" ]; then
+  mvn package
+fi
+
+java -jar "$JAR_ARTIFACT_PATH" "$JAR_ARTIFACT_PATH" -o "$OBF_ARTIFACT_PATH" \
+    --bsm-template 'native/bootstrap.c.ftl' > native/bootstrap.c
+
+cd native/cmake/
+if [ ! -f 'Makefile' ]; then
+  cmake ..
+fi
+make
+mv libbootstrap.so ../../
+
+cd ../../
+java -jar "$OBF_ARTIFACT_PATH"
